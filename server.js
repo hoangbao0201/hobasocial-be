daa8bfd9810e11f9be03a77f54797aff6cba6bb9
hoangbao0201 <hoangbao020103@gmail.com>
2022-10-04 22:54:31 +0700

    Edit connect socket.io

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: [
+            "http://localhost:3000",
+            "https://hoangbao-social-network.vercel.app",
+        ],
     },
 });
 
