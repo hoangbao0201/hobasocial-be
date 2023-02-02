f0594ab7ee94f6bfc9e04d22ab0a32ceeb73d8ee
Nguyễn Hoàng Bảo <hoangbao020103@gmail.com>
2023-02-02 09:06:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://hoangbao-social-network.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // socket
 const http = require("http");
 const server = http.createServer(app);
